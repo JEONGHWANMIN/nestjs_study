3a64bd61048cd104db4d309e511f678099040a9b
JEONGHWANMIN <mimi0j0j@naver.com>
2022-03-28 22:00:24 +0900

    TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { TypeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [BoardsModule, TypeOrmModule.forRoot(TypeORMConfig)],
 })
 export class AppModule {}
